b211f8de734ceaf128b82565614b854e6984041d
Harshkumar12014013 <101981203+Harshkumar12014013@users.noreply.github.com>
2023-07-11 22:29:24 +0530

    final code
    ecommercestore

---
+++ config/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import colors from 'colors';
+
+const connectDB = async() =>{
+     try {
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`Connected To Mongodb ${conn.connection.host}`.bgMagenta.white)
+     } catch (error) {
+     }
+}
+
+export default connectDB;
